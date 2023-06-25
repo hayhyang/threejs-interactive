fa7babe033f4b06aec3f114ac4c402085d4b17bc
hay <hayhyang@gmail.com>
2023-06-25 14:47:24 +0900

    feat: 카메라 마우스로 제어

--- section2/js/01_default.js
+++ section2/js/01_default.js
@@ -1,4 +1,5 @@
-import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.153.0/three.js";
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.153.0/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.108.0/examples/jsm/controls/OrbitControls.js";
 
 let W = window.innerWidth;
 let H = window.innerHeight;
@@ -13,16 +14,39 @@ renderer.setSize(W, H);
 renderer.setClearColor(0x0e2255); // 배경색을 지정할 수 있음,  기본은 블랙
 document.body.appendChild(renderer.domElement);
 
-const axes = new THREE.AxesHelper();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // 부드러운 컨트롤 효과
+
+const axes = new THREE.AxesHelper(50);
 scene.add(axes);
 
-const gridHelper = new THREE.GridHelper();
+const gridHelper = new THREE.GridHelper(70, 20);
 scene.add(gridHelper);
 
+// 조명
+const hemisphereLight = new THREE.HemisphereLight(0xc0daf5, 0xc0daf5, 0.3);
+hemisphereLight.position.set(-50, -50, -50);
+scene.add(hemisphereLight);
+
+const spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(60, 60, 60);
+scene.add(spotLight);
+
+const geometry = new THREE.BoxBufferGeometry(10, 10, 10);
+const material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
+const boxMesh = new THREE.Mesh(geometry, material);
+scene.add(boxMesh);
+
 const animate = () => {
-  camera.lookAt(secne.position); // 장면의 위치를 바라봄
+  controls.update();
+
+  boxMesh.rotation.z += 0.01;
+  boxMesh.rotation.x += 0.01;
+  boxMesh.rotation.y += 0.01;
+
+  camera.lookAt(scene.position); // 장면의 위치를 바라봄
   camera.updateProjectionMatrix(); // 변경된 값을 카메라에 적용한다.
-  renderer(scene, camera);
+  renderer.render(scene, camera);
   requestAnimationFrame(animate);
 };
 
